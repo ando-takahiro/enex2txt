df62c353c9860cda3c6703e10aec90f6134a7fe8
ando-takahiro <takahiro.ando@gmail.com>
2019-02-02 18:38:53 +0900

    feat: local convert on mac

--- enex2txt.py
+++ enex2txt.py
@@ -1,9 +1,21 @@
+import argparse
+import os
 import re
 import sys
+
 from xml.etree import ElementTree as etree
 from inscriptis import get_text
 
-root = etree.parse(sys.stdin)
-notes = root.findall('note')
-for n in notes:
-    sys.stdout.write(get_text(n.find('content').text))
+parser = argparse.ArgumentParser(description='single .enex convert')
+parser.add_argument('--src', help='.enex source file')
+parser.add_argument('--dest', help='.txt directory')
+parser.add_argument('--dest-ext', help='suffix for .txt file titles', default='.raw.txt')
+
+args = parser.parse_args()
+
+root = etree.parse(args.src)
+
+for n in root.findall('note'):
+    title = n.find('title').text
+    with open(os.path.join(args.dest, title + args.dest_ext), 'w') as dest:
+        dest.write(get_text(n.find('content').text))
